cebedbe1adb4db2df0817997417c1b185adcf784
Your Name <you@example.com>
2022-06-21 14:50:41 +0300

    Draw line and say hello to first triangle

--- Source/main.c
+++ Source/main.c
@@ -2,9 +2,15 @@
 #include "stb_image_write.h"
 #include "cglm/include/cglm/affine.h"
 
-#define WIDTH 128
-#define HEIGHT 128
+#define WIDTH 512
+#define HEIGHT 512
 #define NUMBER_OF_CHANNELS 3
+
+typedef struct
+{
+	int x;
+	int y;
+}Point;
 
 void createBuffer(int width, int height, unsigned char** data)
 {
@@ -35,30 +41,37 @@ void setPixel(int red, int green, int blue, int x, int y, unsigned char* data)
 	data[x * WIDTH * NUMBER_OF_CHANNELS + y * NUMBER_OF_CHANNELS + 2] = blue;
 }
 
-void getCirclePoints(unsigned char* data)
+void drawLine(int red, int green, int blue, Point start, Point end, unsigned char* data)
 {
-	float angle = 0.0f;
-	float centerX = (float)WIDTH / 2.0f;
-	float centerY = (float)HEIGHT / 2.0f;
-	float radius = (float)WIDTH / 4.0f;
 	int x, y;
-	for (size_t i = 0; i < 720; i++)
+	for (float increment = 0.0f; increment < 1.0f; increment += 0.001f)
 	{
-		x = centerX + radius * cos(angle);
-		y = centerY + radius * sin(angle);
-		angle += 1.0f;
-		setPixel(255, 0, 0, x, y, data);
+		x = start.x + (end.x - start.x) * increment;
+		y = start.y + (end.y - start.y) * increment;
+		setPixel(red, green, blue, x, y, data);
 	}
+}
+
+void drawTriangle(int red, int green, int blue, 
+	              Point point1, Point point2, Point point3, unsigned char* data)
+{
+	drawLine(red, green, blue, point1, point2, data);
+	drawLine(red, green, blue, point2, point3, data);
+	drawLine(red, green, blue, point3, point1, data);
 }
 
 int main()
 {
 	unsigned char* data = 0;
 	createBuffer(WIDTH, HEIGHT, &data);
-	clearColor(102, 7, 185, data);
+	clearColor(0, 0, 0, data);
 	
-	getCirclePoints(data);
-	writelmage("ozgis_output.png", WIDTH, HEIGHT, 3, data, WIDTH * NUMBER_OF_CHANNELS, true);
+	Point trianglePoint1 = { 200, 200 };
+	Point trianglePoint2 = { 200, 300 };
+	Point trianglePoint3 = { 300, 250 };
+	drawTriangle(255, 255, 0, trianglePoint1, trianglePoint2, trianglePoint3, data);
+	writelmage("triangle.png", WIDTH, HEIGHT, 3, data, 
+		       WIDTH * NUMBER_OF_CHANNELS, true);
 	printf("My first graphic app without OpenGL :) \n");
 
 	return 0;
