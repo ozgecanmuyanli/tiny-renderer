9c495fc78140105137e9c3a3ea566ee13ae1300e
Your Name <you@example.com>
2022-07-01 10:06:28 +0300

    Draw filled triangles using Barycentric Coords

--- Source/main.c
+++ Source/main.c
@@ -1,8 +1,11 @@
 #include <glad/glad.h>
 #include <glfw-3.3.7/include/GLFW/glfw3.h>
+#include <cglm/include/cglm/vec2.h>
+#include <cglm/include/cglm/vec3.h>
 #include <stdio.h>
 #include "stb_image_write.h"
 #include <stdlib.h>
+#include "math.h"
 #include "swap.h"
 #include "loader.h"
 #include "common.h"
@@ -45,6 +48,32 @@ int isInNDC(PointF point)
 		return 1;
 	else
 		return 0;
+}
+
+// Compute barycentric coordinates (u, v, w) for
+// point p with respect to triangle (a, b, c)
+void Barycentric(ivec2 p, ivec2 a, ivec2 b, ivec2 c, vec3 barycentricCoords)
+{
+	float u, v, w;
+    vec2 v0, v1, v2;
+	v0[0] = b[0] - a[0];
+	v0[1] = b[1] - a[1];
+	v1[0] = c[0] - a[0];
+	v1[1] = c[1] - a[1];
+	v2[0] = p[0] - a[0];
+	v2[1] = p[1] - a[1];
+	float d00 = glm_vec2_dot(v0, v0);
+	float d01 = glm_vec2_dot(v0, v1);
+	float d11 = glm_vec2_dot(v1, v1);
+	float d20 = glm_vec2_dot(v2, v0);
+	float d21 = glm_vec2_dot(v2, v1);
+	float denom = d00 * d11 - d01 * d01;
+	v = (d11 * d20 - d01 * d21) / denom;
+	w = (d00 * d21 - d01 * d20) / denom;
+	u = 1.0f - v - w;
+	barycentricCoords[0] = u;
+	barycentricCoords[1] = v;
+	barycentricCoords[2] = w;
 }
 
 void setPixel(int red, int green, int blue, int x, int y, unsigned char* data)
@@ -99,12 +128,51 @@ void drawLine(int red, int green, int blue, PointF start, PointF end, unsigned c
 	}
 }
 
-void drawTriangle(int red, int green, int blue, 
-	              PointF point1, PointF point2, PointF point3, unsigned char* data)
+void drawTriangle(int red, int green, int blue,
+	PointF point1, PointF point2, PointF point3, unsigned char* data, int isFilled)
 {
-	drawLine(red, green, blue, point1, point2, data);
-	drawLine(red, green, blue, point2, point3, data);
-	drawLine(red, green, blue, point3, point1, data);
+	if (isFilled)
+	{
+		if (isInNDC(point1) && isInNDC(point2) && isInNDC(point3))
+		{
+			vec3 bc_screen;
+			ivec2 pixels;
+			Point screenP1, screenP2, screenP3;
+			setViewPort(point1, &screenP1);
+			setViewPort(point2, &screenP2);
+			setViewPort(point3, &screenP3);
+
+			ivec2 p1, p2, p3;
+			p1[0] = screenP1.x;
+			p1[1] = screenP1.y;
+			p2[0] = screenP2.x;
+			p2[1] = screenP2.y;
+			p3[0] = screenP3.x;
+			p3[1] = screenP3.y;
+
+			/* get the bounding box of the triangle */
+			int maxX = max(p1[0], max(p2[0], p3[0]));
+			int minX = min(p1[0], min(p2[0], p3[0]));
+			int maxY = max(p1[1], max(p2[1], p3[1]));
+			int minY = min(p1[1], min(p2[1], p3[1]));
+			for (pixels[0] = minX; pixels[0] <= maxX; pixels[0]++)
+			{
+				for (pixels[1] = minY; pixels[1] <= maxY; pixels[1]++)
+				{
+					Barycentric(pixels, p1, p2, p3, bc_screen);
+					if (bc_screen[0] < 0 || bc_screen[1] < 0 || bc_screen[2] < 0) 
+						continue;
+					setPixel(red, green, blue, pixels[0], pixels[1], data);
+				}
+			}
+		}
+	}
+	else
+	{
+		drawLine(red, green, blue, point1, point2, data);
+		drawLine(red, green, blue, point2, point3, data);
+		drawLine(red, green, blue, point3, point1, data);
+	}
 }
 
 int main()
@@ -114,24 +182,27 @@ int main()
 	clearColor(0, 0, 0, data);
 
 	int numOfTriangles = LoadObjAndConvert("../../Resources/african_head.obj");
-	if (0 == numOfTriangles) 
+	if (0 == numOfTriangles)
 	{
 		printf("\nfailed to load & conv\n");
 		return -1;
 	}
 	for (size_t i = 0; i < numOfTriangles; i++)
 	{
-		drawTriangle(255, 255, 0, vertexArray[0 + i * 3], 
-								  vertexArray[1 + i * 3], 
-			                      vertexArray[2 + i * 3], 
-			                      data);
+		int r = rand() % 255;
+		int g = rand() % 255;
+		int b = rand() % 255;
+		drawTriangle(r, g, b, vertexArray[0 + i * 3],
+			vertexArray[1 + i * 3],
+			vertexArray[2 + i * 3],
+			data, 1);
 	}
 
-	writeImage("../../../output_images/african.png", WIDTH, HEIGHT,
+	writeImage("../../../output_images/colored_filled_african_head.png", WIDTH, HEIGHT,
 		3, data, WIDTH * NUMBER_OF_CHANNELS, 1);
 
 	textureData = data;
-	OpenWindow(WIDTH/2, HEIGHT/2);
+	OpenWindow(WIDTH / 2, HEIGHT / 2);
 	MainLoop();
 
 	free(data);
--- Source/main.h
+++ Source/main.h
@@ -9,6 +9,7 @@ void clearColor(int red, int green, int blue, unsigned char* data);
 void setPixel(int red, int green, int blue, int x, int y, unsigned char* data);
 void drawLine(int red, int green, int blue, PointF start, PointF end, unsigned char* data);
 void drawTriangle(int red, int green, int blue,
-	PointF point1, PointF point2, PointF point3, unsigned char* data);
+	PointF point1, PointF point2, PointF point3, unsigned char* data, int isFilled);
 void setViewPort(PointF point, Point* screenPoint);
 int isInNDC(PointF point);
+//void Barycentric(ivec2 p, ivec2 a, ivec2 b, ivec2 c, vec3 barycentricCoords);
